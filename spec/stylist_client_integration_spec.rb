f669e44dc79e1ffc9f72439add11de91b9b35491
Meaghan Jones <meaghan.m.jones@gmail.com>
2016-07-15 14:23:24 -0700

    add paths in the app file to pass the integration spec of adding a new salon

--- spec/stylist_client_integration_spec.rb
+++ spec/stylist_client_integration_spec.rb
@@ -7,8 +7,8 @@ set(:show_exceptions, false)
 describe('adding a new salon', {:type => :feature}) do
   it "allows a user to add a new stylist to the list" do
     visit('/')
-    click_button('Add Stylist')
     fill_in('stylist_name', :with => 'Ruby')
+    click_button('Add Stylist')
     expect(page).to have_content('Success')
   end
 end
